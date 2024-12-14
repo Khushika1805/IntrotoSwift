4d036e1e16c736fed4951599901a0c281113efec
Khushika1805 <kshah7@umbc.edu>
2024-12-14 00:04:56 -0500

    adding swift files

---
+++ codepathprojTests/codepathprojTests.swift
@@ -0,0 +1,17 @@
+//
+//  codepathprojTests.swift
+//  codepathprojTests
+//
+//  Created by Khushika Shah on 12/13/24.
+//
+
+import Testing
+@testable import codepathproj
+
+struct codepathprojTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
